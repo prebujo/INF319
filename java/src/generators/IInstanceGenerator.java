c00f10328cae12909efac28aac743492610ae452
prebujo <prebujo@gmail.com>
2019-06-22 02:47:03 +0200

    instance generator part 14 done

--- java/src/generators/IInstanceGenerator.java
+++ java/src/generators/IInstanceGenerator.java
@@ -5,7 +5,10 @@ public interface IInstanceGenerator {
      *
      * @param orderAmount
      * @param vehicleAmount
-     * @param locationAmount
+     * @param pickupLocationAmount
+     * @param deliveryLocationAmount
+     * @param factoryAmount
+     * @param instanceID
      * @param instanceType is the type of instance, 1 = international (europe), 2= local (germany), 3 = uniform distribution
      */
     void makeInstance(int orderAmount, int vehicleAmount, int pickupLocationAmount, int deliveryLocationAmount, int factoryAmount, int instanceID, int instanceType);
--- java/src/generators/InstanceGenerator.java
+++ java/src/generators/InstanceGenerator.java
@@ -5,9 +5,19 @@ import java.io.IOException;
 import java.util.Random;
 
 public class InstanceGenerator implements IInstanceGenerator {
+    private static final double ORDER_WEIGHT_LOWER_BOUND = 50;
+    private static final double ORDER_WEIGHT_MEDIUM_BOUND = 2000;
+    private static final double ORDER_WEIGHT_UPPER_BOUND = 24000;
+    private static final double ORDER_VOLUME_PORTION_LOWER_BOUND = 4;
+    private static final double ORDER_VOLUME_PORTION_MEDIUM_BOUND = 265;
+    private static final double ORDER_VOLUME_PORTION_UPPER_BOUND = 1000;
+    private static final double ORDER_VOLUME_MEDIUM_BOUND = 50;
+    private static final double ORDER_VOLUME_UPPER_BOUND = 100;
     private final Random random;
     private final int COST_STRUCTURE_DIMENSION_LOWER_BOUND = 2;
     private final int COST_STRUCTURE_DIMENSION_UPPER_BOUND = 7;
+    private final double MINIMUM_COST_OF_NO_TRANSPORT = 20000;
+    private final double MAXIMUM_COST_OF_NO_TRANSPORT = 40000;
 
     public InstanceGenerator(){
         this.random = new Random(13);
@@ -22,32 +32,23 @@ public class InstanceGenerator implements IInstanceGenerator {
 
             int itemNumber = 1;
 
-            String parameterName = "nv";
-            String parameterDescription = "amount of Vehicles";
-            printNumber(itemNumber++, vehicleAmount, parameterName, parameterDescription, fileWriter);
+            printNumber(itemNumber++, vehicleAmount, "nv", "amount of Vehicles", fileWriter);
 
-            parameterName = "no";
-            parameterDescription = "amount of Orders";
-            printNumber(itemNumber++,orderAmount,parameterName,parameterDescription,fileWriter);
+            printNumber(itemNumber++,orderAmount,"no","amount of Orders",fileWriter);
 
             int totalLocationsAmount = pickupLocationAmount+deliveryLocationAmount;
-            parameterName = "nl";
-            parameterDescription = "amount of locations";
-            printNumber(itemNumber++,totalLocationsAmount,parameterName,parameterDescription,fileWriter);
 
-            parameterName = "nf";
-            parameterDescription = "amount of factories";
-            printNumber(itemNumber++,factoryAmount,parameterName,parameterDescription,fileWriter);
+            printNumber(itemNumber++,totalLocationsAmount,"nl","amount of locations",fileWriter);
 
-            parameterName = "na";
-            parameterDescription = "amount of distance intervals";
+            printNumber(itemNumber++,factoryAmount,"nf","amount of factories",fileWriter);
 
-            int[] distanceIntervals = oneDimensionTable(vehicleAmount, itemNumber++,COST_STRUCTURE_DIMENSION_LOWER_BOUND,COST_STRUCTURE_DIMENSION_UPPER_BOUND, parameterName, parameterDescription, fileWriter);
+            writeNewParameterSection(itemNumber++,"na","amount of distance intervals",fileWriter);
 
-            parameterName = "nb";
-            parameterDescription = "amount of weight intervals";
+            int[] distanceIntervals = oneDimensionTable(vehicleAmount, COST_STRUCTURE_DIMENSION_LOWER_BOUND,COST_STRUCTURE_DIMENSION_UPPER_BOUND, fileWriter);
 
-            int[] weightIntervals = oneDimensionTable(vehicleAmount,itemNumber++,COST_STRUCTURE_DIMENSION_LOWER_BOUND,COST_STRUCTURE_DIMENSION_UPPER_BOUND,parameterName,parameterDescription,fileWriter);
+            writeNewParameterSection(itemNumber++,"nb","amount of weight intervals",fileWriter);
+
+            int[] weightIntervals = oneDimensionTable(vehicleAmount,COST_STRUCTURE_DIMENSION_LOWER_BOUND,COST_STRUCTURE_DIMENSION_UPPER_BOUND,fileWriter);
 
             int[] factoryLocationAmounts = new int[factoryAmount];
 
@@ -57,30 +58,79 @@ public class InstanceGenerator implements IInstanceGenerator {
                 counter--;
             }
 
+
             while (counter>0){
-                factoryLocationAmounts[random.nextInt(factoryAmount)]++;
+                int selected = random.nextInt(factoryAmount);
+                factoryLocationAmounts[selected]++;
                 counter--;
             }
 
-            parameterName = "N_f_2";
-            parameterDescription = "factory sets";
 
-            fileWriter.write("# "+itemNumber+" "+ parameterDescription+"\n");
-            int location = pickupLocationAmount+1;
+            writeHeaderSection(itemNumber++, "factory sets", fileWriter);
+
+            String setName = "N_f_2";
+            int[] locationElements = createPossibleElementsTable(deliveryLocationAmount,pickupLocationAmount+1);
+            int locationElementsCounter = deliveryLocationAmount;
             for (int i = 0;i<factoryAmount;i++) {
-                fileWriter.write("set " + parameterName + "["+(i+1)+"] := ");
+                writeSetNumber(setName, i, fileWriter);
                 counter=factoryLocationAmounts[i];
                 while(counter>0){
-                    fileWriter.write(" "+location++);
+                    int choiceIdx = random.nextInt(locationElementsCounter--);
+                    fileWriter.write(" "+locationElements[choiceIdx]);
+                    if(choiceIdx!=locationElementsCounter){
+                        locationElements[choiceIdx] = locationElements[locationElementsCounter];
+                    }
                     counter--;
                 }
                 fileWriter.write(" ;\n");
             }
             fileWriter.write("\n");
 
+            writeNewParameterSection(itemNumber++,"H","factory stop capacities",fileWriter);
+            for (int i = 0;i<factoryAmount-1;i++){
+                fileWriter.write((i+1)+" "+(1+random.nextInt(factoryLocationAmounts[i]))+"\n");
+            }
+            fileWriter.write(factoryAmount+" "+(1+random.nextInt(factoryLocationAmounts[factoryAmount-1]))+" ;\n\n");
 
+            writeNewParameterSection(itemNumber++,"L_pd","pickup locations for each order",fileWriter);
 
+            int[] pickupLocation = oneDimensionTableWithRandomElements(orderAmount, pickupLocationAmount, 1, fileWriter);
 
+            writeNewParameterSection(itemNumber++, "L_dd", "delivery locations for each order", fileWriter);
+
+            int[] deliveryLocation = oneDimensionTableWithRandomElements(orderAmount, deliveryLocationAmount,pickupLocationAmount+1, fileWriter);
+
+            int[][] locationOrderSet = new int[totalLocationsAmount+1][orderAmount];
+            int[] locationOrderSetCounter = new int[totalLocationsAmount+1];
+            for(int i = 0;i<orderAmount;i++) {
+                locationOrderSet[pickupLocation[i]][locationOrderSetCounter[pickupLocation[i]]++] = i+1;
+            }
+            for(int i = 0;i<orderAmount;i++) {
+                locationOrderSet[deliveryLocation[i]][locationOrderSetCounter[deliveryLocation[i]]++] = i+1;
+            }
+
+            writeHeaderSection(itemNumber++,"location sets",fileWriter);
+            setName = "L_2";
+            for(int i = 0;i<totalLocationsAmount;i++){
+                writeSetNumber(setName, i, fileWriter);
+                int j = 0;
+                while(locationOrderSet[i+1][j]!=0){
+                    fileWriter.write(" "+locationOrderSet[i+1][j++]);
+                }
+                fileWriter.write(" ;\n");
+            }
+            fileWriter.write("\n");
+
+            double vehicleVolumeMax = 102;
+
+            writeNewParameterSection(itemNumber++,"Q_kg_2","order weights",fileWriter);
+            double[] orderWeights = oneDimensionTable(orderAmount,ORDER_WEIGHT_LOWER_BOUND,ORDER_WEIGHT_MEDIUM_BOUND,ORDER_WEIGHT_UPPER_BOUND,fileWriter);
+
+            writeNewParameterSection(itemNumber++,"Q_vol_2","order volumes",fileWriter);
+            double[] orderVolumes = oneDimensionTable(orderAmount,ORDER_VOLUME_PORTION_LOWER_BOUND,ORDER_VOLUME_PORTION_MEDIUM_BOUND,ORDER_VOLUME_PORTION_UPPER_BOUND, vehicleVolumeMax, fileWriter, orderWeights);
+
+            writeNewParameterSection(itemNumber++,"C_2", "cost of no transport",fileWriter);
+            double[] costOfNoTransport = oneDimensionTable(orderAmount,MINIMUM_COST_OF_NO_TRANSPORT,MAXIMUM_COST_OF_NO_TRANSPORT,MAXIMUM_COST_OF_NO_TRANSPORT,fileWriter);
 
 
             fileWriter.close();
@@ -91,12 +141,60 @@ public class InstanceGenerator implements IInstanceGenerator {
 
     }
 
-    private int[] oneDimensionTable(int tableDimension, int itemNumber, int lowerBound, int upperBound, String parameterName, String parameterDescription, FileWriter fileWriter) throws IOException {
-        int[] table = new int[tableDimension];
-        fileWriter.write("# "+itemNumber+" "+ parameterDescription+"\n");
+    private void writeSetNumber(String outputName, int i, FileWriter fileWriter) throws IOException {
+        fileWriter.write("set " + outputName + "["+(i+1)+"] := ");
+    }
+
+    private void writeHeaderSection(int itemNumber, String outputDescription, FileWriter fileWriter) throws IOException {
+        fileWriter.write("# " + (itemNumber) + " " + outputDescription + "\n");
+    }
+
+    private void writeNewParameterSection(int itemNumber, String parameterName, String parameterDescription, FileWriter fileWriter) throws IOException {
+        writeHeaderSection(itemNumber++, parameterDescription, fileWriter);
+
         fileWriter.write("param "+parameterName+" :=\n");
+    }
+
+    private int[] oneDimensionTableWithRandomElements(int tabelSize, int notRandomLimit, int startElement, FileWriter fileWriter) throws IOException {
+        int[] result = new int[tabelSize];
+
+        int[] elements = createPossibleElementsTable(notRandomLimit, startElement);
+        int counter=notRandomLimit;
+        int order = 0;
+        while(counter>0&&order<tabelSize-1){
+            int choiceIdx = random.nextInt(counter--);
+            result[order++] = elements[choiceIdx];
+            if(choiceIdx!=counter){
+                elements[choiceIdx] = elements[counter];
+            }
+            fileWriter.write((order)+" "+result[order-1]+"\n");
+        }
+        while (order<tabelSize-1){
+            result[order++]=startElement+random.nextInt(notRandomLimit);
+            fileWriter.write((order)+" "+result[order-1]+"\n");
+        }
+        if (counter>0){
+            result[order++] = elements[0];
+            fileWriter.write((order)+" "+result[order-1]+" ;\n\n");
+        } else{
+            result[order++]=startElement+random.nextInt(notRandomLimit);
+            fileWriter.write((order)+" "+result[order-1]+" ;\n\n");
+        }
+        return result;
+    }
+
+    private int[] createPossibleElementsTable(int tableSize, int startElement) {
+        int[] elements = new int[tableSize];
+        for(int i  = 0;i<tableSize;i++){
+            elements[i] = startElement+i;
+        }
+        return elements;
+    }
+
+    private int[] oneDimensionTable(int tableDimension, int lowerBound, int upperBound, FileWriter fileWriter) throws IOException {
+        int[] table = new int[tableDimension];
         for (int i = 0;i<tableDimension-1;i++){
-            table[i] = lowerBound+random.nextInt(upperBound-lowerBound);
+            table[i] = lowerBound+random.nextInt(upperBound-lowerBound+1);
             fileWriter.write((i+1)+" "+table[i]+"\n");
         }
         table[tableDimension-1] = lowerBound+random.nextInt(upperBound-lowerBound+1);
@@ -104,9 +202,50 @@ public class InstanceGenerator implements IInstanceGenerator {
         return table;
     }
 
+    private double[] oneDimensionTable(int tableDimension, double lowerBound, double mediumBound, double upperBound, FileWriter fileWriter) throws IOException {
+        double[] table = new double[tableDimension];
+        for (int i = 0;i<tableDimension-1;i++){
+            table[i] = lowerBound+random.nextDouble()*(mediumBound-lowerBound);
+            if(random.nextDouble()>0.9){
+                table[i]+=random.nextDouble()*(upperBound-mediumBound);
+            }
+            fileWriter.write((i+1)+" "+table[i]+"\n");
+        }
+        table[tableDimension-1] = lowerBound+random.nextDouble()*(upperBound-lowerBound);
+        if(random.nextDouble()>0.9){
+            table[tableDimension-1]+=random.nextDouble()*(upperBound-mediumBound);
+        }
+        fileWriter.write(tableDimension+" "+table[tableDimension-1]+" ;\n\n");
+        return table;
+    }
+
+    private double[] oneDimensionTable(int tableDimension, double lowerBound, double mediumBound, double upperBound, double vehicleVolumeMax, FileWriter fileWriter, double[] regulator) throws IOException {
+        double[] table = new double[tableDimension];
+        for (int i = 0;i<tableDimension-1;i++){
+            table[i] = lowerBound+random.nextDouble()*(mediumBound-lowerBound);
+            if(random.nextDouble()>0.9){
+                table[i]+=random.nextDouble()*(upperBound-mediumBound);
+            }
+            table[i]=regulator[i]/table[i];
+            if(table[i]>vehicleVolumeMax){
+                table[i] = ORDER_VOLUME_MEDIUM_BOUND+random.nextDouble()*(vehicleVolumeMax-ORDER_VOLUME_MEDIUM_BOUND);
+            }
+            fileWriter.write((i+1)+" "+table[i]+"\n");
+        }
+        table[tableDimension-1] = lowerBound+random.nextDouble()*(upperBound-lowerBound);
+        if(random.nextDouble()>0.9){
+            table[tableDimension-1]+=random.nextDouble()*(upperBound-mediumBound);
+        }
+        table[tableDimension-1]=regulator[tableDimension-1]/table[tableDimension-1];
+        if(table[tableDimension-1]>vehicleVolumeMax){
+            table[tableDimension-1] = ORDER_VOLUME_MEDIUM_BOUND+random.nextDouble()*(vehicleVolumeMax-ORDER_VOLUME_MEDIUM_BOUND);
+        }
+        fileWriter.write(tableDimension+" "+table[tableDimension-1]+" ;\n\n");
+        return table;
+    }
+
     private void printNumber(int itemNumber, int outputNumber, String parameterName, String parameterDescription, FileWriter fileWriter) throws IOException {
-        fileWriter.write("# "+itemNumber+" "+ parameterDescription+"\n");
-        fileWriter.write("param "+parameterName+" :=\n");
+        writeNewParameterSection(itemNumber++,parameterName,parameterDescription,fileWriter);
         fileWriter.write(outputNumber+" ;\n\n");
 
     }
